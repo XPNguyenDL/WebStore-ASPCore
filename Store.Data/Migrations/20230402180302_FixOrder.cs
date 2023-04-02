77819fe5d8299222313873967c0bbedd3c433f04
XPNguyenDL <xpnguyen240802@gmail.com>
2023-04-03 01:21:58 +0700

    Add DataSeeder

---
+++ Store.Data/Migrations/20230402180302_FixOrder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Store.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Total",
+                table: "Orders",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
