e6242a85805f6c3598fc81e13a3ea256e10c19f2
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-05-02 22:20:04 +0700

    Add paging model

--- Store.WebAPI/Models/PagingModel.cs
+++ Store.WebAPI/Models/PagingModel.cs
@@ -4,8 +4,8 @@ namespace Store.WebAPI.Models;
 
 public class PagingModel : IPagingParams
 {
-	public int PageNumber { get; set; } = 1;
-	public int PageSize { get; set; } = 10;
+	public int? PageNumber { get; set; } = 1;
+	public int? PageSize { get; set; } = 10;
 	public string SortColumn { get; set; } = "Id";
 	public string SortOrder { get; set; } = "DESC";
 }
